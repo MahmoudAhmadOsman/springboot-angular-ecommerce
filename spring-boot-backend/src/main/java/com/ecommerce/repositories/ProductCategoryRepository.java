aebda82c13a132aaf25a4cf2b8296e3419804269
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-08-21 15:17:03 -0500

    disabled PUT, POST & DELETE methods

--- spring-boot-backend/src/main/java/com/ecommerce/repositories/ProductCategoryRepository.java
+++ spring-boot-backend/src/main/java/com/ecommerce/repositories/ProductCategoryRepository.java
@@ -2,6 +2,6 @@ package com.ecommerce.repositories;
 
 import com.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//Interface Class
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
