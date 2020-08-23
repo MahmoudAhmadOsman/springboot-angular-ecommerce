74a47699ce3fb194325e145281ee6bec8fadd246
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-08-23 02:25:40 -0500

    added ProductService class & product component

--- frontend/angular-ecommerce/src/app/common/product.ts
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,8 +1,7 @@
 export class Product {
   //Here is where you list all the properties that match the Java Class [Product] in the backend in Spring Boot
-
-  sku: String;
   name: String;
+  sku: String;
   description: String;
   unitPrice: Number;
   imageUrl: String;
